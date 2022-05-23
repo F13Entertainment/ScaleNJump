c70532fbac9b0e2b19e05a754a0f187f0e2286ec
GorkemUnerF13 <gorkem.uner@f13entertainment.com>
2022-05-23 16:50:29 +0300

    -Unnecessary lines are removed.
    -Unnecessary lines are removed.

--- Assets/_GAME/Scripts/CameraController.cs
+++ Assets/_GAME/Scripts/CameraController.cs
@@ -1,5 +1,4 @@
 using DG.Tweening;
-using F13StandardUtils.Scripts.Core;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -10,9 +9,7 @@ public class CameraController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //cam?.transform.DOKill();
         Lerp(Mathf.Clamp((60f + Ball.Instance.Rb.velocity.magnitude / 3f), 60f, 70f));
-        //cam?.DOFieldOfView(Mathf.Clamp((60f + Ball.Instance.Rb.velocity.magnitude / 3f), 60f, 70f), .5f);
         transform.position = Vector3.SmoothDamp(transform.position, new Vector3(Ball.Instance.transform.position.x, transform.position.y, transform.position.z) + Offset, ref velocity, .55f);
     }
 
@@ -23,43 +20,6 @@ public class CameraController : MonoBehaviour
 
     private void OnDestroy()
     {
-        Debug.Log("OnDestroy FİXED E GİRDİM");
         cam?.transform.DOKill();
     }
 }
-
-//using UnityEngine;
-
-//public class CameraController : MonoBehaviour
-//{
-//    [SerializeField] private Camera cam;
-//    [SerializeField] private Vector3 Offset;
-//    private Vector3 velocity = Vector3.zero;
-
-//    private void FixedUpdate()
-//    {
-//        cam.fieldOfView = Mathf.SmoothDamp(cam.fieldOfView, Mathf.Clamp((60f + Ball.Instance.Rb.velocity.magnitude / 3f), 60f, 70f),ref velocity.x,.5f);
-//        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(Ball.Instance.transform.position.x, transform.position.y, transform.position.z) + Offset, ref velocity, .55f);
-//    }
-
-//    //void LateUpdate()
-//    //{
-//    //    //Debug.Log("Mathf.Clamp((60f + Ball.Instance.Rb.velocity.magnitude), 60f, 70f): " + Mathf.Clamp((60f + Ball.Instance.Rb.velocity.magnitude/3), 60f, 70f));
-
-//    //    //cam.fieldOfView = Mathf.SmoothDamp(cam.fieldOfView, Mathf.Clamp((60f + Ball.Instance.Rb.velocity.magnitude), 60f, 70f), ref velocity.x, 1f);
-//    //    //transform.position =  Vector3.SmoothDamp(transform.position, new Vector3(Ball.Instance.transform.position.x, transform.position.y, transform.position.z) + Offset, ref velocity, .55f);
-//    //}
-
-//    ////private void LerpFieldOfView()
-//    ////{
-
-//    ////    // Distance moved equals elapsed time times speed..
-//    ////    float distCovered = (Time.time - startTime) * speed;
-
-//    ////    // Fraction of journey completed equals current distance divided by total distance.
-//    ////    float fractionOfJourney = distCovered / journeyLength;
-
-//    ////    // Set our position as a fraction of the distance between the markers.
-//    ////    transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
-//    ////}
-//}
--- Assets/_GAME/Scripts/FinishCollider.cs
+++ Assets/_GAME/Scripts/FinishCollider.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using F13StandardUtils.Scripts.Core;
 using UnityEngine;
 
@@ -8,9 +6,6 @@ public class FinishCollider : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Ball")
-        {
             GameController.Instance.SuccessLevel();
-        }
-
     }
 }
